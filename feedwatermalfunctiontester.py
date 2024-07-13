82f5b1e90fe740c05c6d37df6c5aab47923c173d
Malexz <90273612+Male234@users.noreply.github.com>
2024-07-13 17:41:38 +0200

    Update feedwatermalfunctiontester.py
    Made it more compact; thanks to ggwpxdl012 on the RBWR discord ;)

--- feedwatermalfunctiontester.py
+++ feedwatermalfunctiontester.py
@@ -3,7 +3,7 @@
 import math
 print('THIS IS A VERY HIGH TECH TESTER TOTALLY!')
 print('ONLY INPUT NUMBERS UNLESS SAID OTHERWISE')
-print('DISCLAIMER: THIS IS NOT VERY ACCURATE AND YOU SHOULD SCRAM THE REACTOR TO TEST IT PROPERLY \n IF THE NUMBER YOU INPUT IS NOT A MULTIPLY OF 10 THEN IT WILL NOT WORK PROPERLY')
+print('DISCLAIMER: THIS IS NOT VERY ACCURATE AND YOU SHOULD SCRAM THE REACTOR TO TEST IT PROPERLY (if very very needed tho) \n IF THE NUMBER YOU INPUT IS NOT A MULTIPLY OF 10 THEN IT WILL NOT WORK PROPERLY')
 
 #variables
 full_flow = 1000
@@ -13,9 +13,9 @@ perc_1 = 10
 
 
 #math
-result = perc_1*pump_perc
+result = pump_flow/pump_perc
 
-if result != full_flow/pump_perc:
+if result != pump_flow/pump_perc*100:
     print("You have a malfunction!")
 else:
     print("There is no malfunction on the pump!")
